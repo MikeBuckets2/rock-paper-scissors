3dcef993c31ee0fa330b4b0afd2a0eb04358e6aa
Michael Morris <michaelmorris121@yahoo.com>
2025-11-02 16:59:43 -0600

    add function for computer choice
    so that computer will randomly choose
    rock paper or scissors

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+// Function name getComputerChoice
+// that generates randomly
+// rock paper or scissors
+// random number 0 to 1
+// to assign to rock paper or scissors
 
+function getComputerChoice () {
+    let choice = Math.random();
+    if (choice < 0.33) {
+        return "rock";
+    } else if (choice < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
 
