0a9cd5cfe88f6068ac1d692ddfc8a3d9b714bfd1
Michael Morris <michaelmorris121@yahoo.com>
2025-11-02 15:48:43 -0600

    add function to track winner

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNum = Math.random(); 
     if (randomNum < 0.33) {
@@ -13,7 +16,31 @@ function getHumanChoice() {
     let userChoice = prompt("rock paper or scissors?");
     return userChoice;
 }
-console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+//Function name playRound
+//  Take human and computer choices as arguments
+//  make human choice case insensitive
+//  display a round winner
+//  increment score for winner
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win, rock beats scissors!");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win, paper beats rock!");
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win, scissors beats paper!");
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        computerScore++;
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
